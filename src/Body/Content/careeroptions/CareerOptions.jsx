6e5b22ed67701d5c032277c814fb5fb64737a7bd
N4ko01 <jesus.vasquez.sandoval.2002@gmail.com>
2025-04-28 02:46:44 -0500

    last deploy

--- src/Body/Content/careeroptions/CareerOptions.jsx
+++ src/Body/Content/careeroptions/CareerOptions.jsx
@@ -6,21 +6,16 @@ const careers = [
         name: "Informática",
         image: "https://saluddata.com/wp-content/uploads/2023/11/que-es-la-informatica.webp",
         path: "/scene_informatica.html"
-
     },
     {
         name: "Electrónica",
         image: "https://serviciostecnicosmovil.com/wp-content/uploads/2019/09/electronica.jpg",
         path: "/scene_electronica.html"
-
-
     },
     {
         name: "Telecomunicaciones",
         image: "https://www.osiptel.gob.pe/media/ozkdsiop/np-telecom.jpg",
         path: "/scene_telecomunicaciones.html"
-
-
     }
 ];
 
